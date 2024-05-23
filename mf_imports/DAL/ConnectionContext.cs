219ba1b751a24be93309e73a68cf05883dd186b1
Marcio Souza <marcio.souza@mps.com.br>
2024-05-23 17:18:37 -0300

    criada conexão com banco de dados

---
+++ mf_imports/DAL/ConnectionContext.cs
@@ -0,0 +1,22 @@
+﻿using mf_imports.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace mf_imports.DAL;
+
+public class ConnectionContext : DbContext
+{
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Conector> Conectores { get; set; }
+    public DbSet<UnidadeMedida> UnidadeMedidas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string server = "mvsr-oneway";
+        string database = "mfautoparts";
+        string user = "api_user";
+        string password = "bHoiwym4oUGArVGh";
+        optionsBuilder.UseSqlServer("Server=mvsr-oneway.database.windows.net;Database=mfautoparts;User=api_user;Password=bHoiwym4oUGArVGh");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
