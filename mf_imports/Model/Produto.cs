417654b9fe92f499e4ec7ce51c1b9c60d514397c
Marcio Souza <marcio.souza@mps.com.br>
2024-05-23 17:21:09 -0300

    adicionadas classes modelo

---
+++ mf_imports/Model/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace mf_imports.Model;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public Categoria Categoria { get; set; }
+    public Conector? Conector { get; set; }
+    public UnidadeMedida UnidadeMedida { get; set; }
+    public decimal PrecoCompra { get; set; }
+    public decimal PrecoVenda { get; set; }
+    public IList<string> OpcoesCompra { get; set; }
+}
