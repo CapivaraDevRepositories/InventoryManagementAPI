b2d4d30bfe8ae2b206233756a2609fc33ab42d08
Marcio Souza <marcio.souza@mps.com.br>
2024-05-23 17:19:13 -0300

    Adidionado Cors e Service de Categoria

--- mf_imports/Program.cs
+++ mf_imports/Program.cs
@@ -1,4 +1,23 @@
+using mf_imports.DAL;
+using mf_imports.DAL.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var OrigensParaCors = "_OrigemParaCors";
+
+builder.Services.AddCors(options =>
+    options.AddPolicy(name: OrigensParaCors,
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:4200",
+                "http://127.0.0.1:4200"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        }
+    )
+);
 
 // Add services to the container.
 
@@ -6,6 +25,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
 var app = builder.Build();
 
