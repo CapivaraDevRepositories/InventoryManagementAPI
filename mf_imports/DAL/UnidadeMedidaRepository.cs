43bd9c7ff1bfb3601638d2631b82d7c4ee3b2367
Marcio Souza <marcio.souza@mps.com.br>
2024-05-23 17:21:43 -0300

    criado repository de Unidade de Medida

---
+++ mf_imports/DAL/UnidadeMedidaRepository.cs
@@ -0,0 +1,41 @@
+﻿using mf_imports.DAL.Interfaces;
+using mf_imports.Model;
+
+namespace mf_imports.DAL;
+
+public class UnidadeMedidaRepository : IUnidadeMedida
+{
+    private ConnectionContext _context = new ConnectionContext();
+    
+    public void Add(UnidadeMedida unidadeMedida)
+    {
+        _context.UnidadeMedidas.Add(unidadeMedida);
+    }
+
+    public List<UnidadeMedida> GetAll()
+    {
+        return _context.UnidadeMedidas.ToList();
+    }
+
+    public UnidadeMedida GetById(int id)
+    {
+        return _context.UnidadeMedidas.FirstOrDefault(um => um.Id == id);
+    }
+
+    public UnidadeMedida GetByName(string name)
+    {
+        return _context.UnidadeMedidas.FirstOrDefault(um => um.Nome.Equals(name));
+    }
+
+    public void Alter(UnidadeMedida unidadeMedida)
+    {
+        _context.UnidadeMedidas.Update(unidadeMedida);
+        _context.SaveChanges();
+    }
+
+    public void Delete(UnidadeMedida unidadeMedida)
+    {
+        _context.Remove(unidadeMedida);
+        _context.SaveChanges();
+    }
+}
