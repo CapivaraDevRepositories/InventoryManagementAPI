1e79ef60cbfc5fe020c4d4c184126e712c2e6213
Marcio Souza <marcio.souza@mps.com.br>
2024-05-23 17:21:23 -0300

    criado repository de produto

---
+++ mf_imports/DAL/ProdutoRepository.cs
@@ -0,0 +1,41 @@
+﻿using mf_imports.DAL.Interfaces;
+using mf_imports.Model;
+
+namespace mf_imports.DAL;
+
+public class ProdutoRepository : IProdutoRepository
+{
+    private ConnectionContext _context = new ConnectionContext();
+    
+    public void Add(Produto produto)
+    {
+        _context.Produtos.Add(produto);
+    }
+
+    public List<Produto> GetAll()
+    {
+        return _context.Produtos.ToList();
+    }
+
+    public Produto GetById(int id)
+    {
+        return _context.Produtos.FirstOrDefault(p => p.Id == id);
+    }
+
+    public Produto GetByName(string name)
+    {
+        return _context.Produtos.FirstOrDefault(p => p.Nome.Equals(name));
+    }
+
+    public void Alter(Produto produto)
+    {
+        _context.Produtos.Update(produto);
+        _context.SaveChanges();
+    }
+
+    public void Delete(Produto produto)
+    {
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+    }
+}
