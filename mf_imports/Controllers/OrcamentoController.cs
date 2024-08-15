23fecd2f67ad681300a2aecffc2db085c50542a6
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-08-15 10:57:33 -0300

    Adicionado Endpoint para realização de orçamentos de acordo com lista de produtos passada como parâmetro

---
+++ mf_imports/Controllers/OrcamentoController.cs
@@ -0,0 +1,31 @@
+﻿using mf_imports.Model;
+using mf_imports.Services;
+using mf_imports.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mf_imports.Controllers;
+
+[ApiController]
+[Route("api/v1/orcamento")]
+public class OrcamentoController : ControllerBase
+{
+    private readonly ICalculoImpostoService _calculoImpostoService;
+
+    public OrcamentoController(ICalculoImpostoService calculoImpostoService)
+    {
+        _calculoImpostoService = calculoImpostoService;
+    }
+
+    [HttpPost]
+    [Route("gerar")]
+    public IActionResult GerarOrcamento([FromBody] List<Produto> listaProdutos)
+    {
+        if (null == listaProdutos || !listaProdutos.Any())
+        {
+            return BadRequest("A lista de produtos não pode estar vazia.");
+        }
+        var orcamento = _calculoImpostoService.CalculoImposto(listaProdutos);
+        return Ok(orcamento);
+    }
+    
+}
