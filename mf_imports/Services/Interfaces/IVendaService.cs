c1984081be73398b3b4197a6765ea933ae32961f
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-08-16 17:24:31 -0300

    criado DTO de Venda

--- mf_imports/Services/Interfaces/IVendaService.cs
+++ mf_imports/Services/Interfaces/IVendaService.cs
@@ -1,10 +1,11 @@
 ﻿using mf_imports.Model;
+using mf_imports.DTO;
 
 namespace mf_imports.Services.Interfaces;
 
 public interface IVendaService
 {
-    public void RealizarVenda(Venda venda);
+    public void RealizarVenda(VendaDTO venda);
     
     public IList<Venda> GetAll();
 }
