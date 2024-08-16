197c9d852e52d32c675027b0b90fc38550227b7c
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-08-16 12:32:41 -0300

    criado endpoint de compras

---
+++ mf_imports/Controllers/ComprasController.cs
@@ -0,0 +1,46 @@
+﻿using mf_imports.Model;
+using mf_imports.DTO;
+using mf_imports.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mf_imports.Controllers;
+
+[ApiController]
+[Route("api/v1/compras")]
+public class ComprasController : ControllerBase
+{
+    private readonly ICompraProdutoService _compraProdutoService;
+
+    public ComprasController(ICompraProdutoService compraProdutoService)
+    {
+        _compraProdutoService = compraProdutoService;
+    }
+
+    [HttpPost]
+    public IActionResult RegistrarCompra([FromBody] IList<ProdutoCompraDTO> compraProduto)
+    {
+        try
+        {
+            _compraProdutoService.RegistrarCompra(compraProduto);
+            return Ok("Compra registrada com sucesso.");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet]
+    public IActionResult GetCompras()
+    {
+        try
+        {
+            List<CompraProduto> comprasRealizadas = _compraProdutoService.GetCompraProdutos();
+            return Ok(comprasRealizadas);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
