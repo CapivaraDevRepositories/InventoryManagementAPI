2ec4d627d4d17e73e1a50b42d334f0fc44cc2b28
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-08-16 12:31:32 -0300

    adicionado objeto DTO para Compra de Produto

---
+++ mf_imports/DTO/ProdutoCompraDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace mf_imports.DTO;
+
+public class ProdutoCompraDTO
+{
+    public int ProdutoId { get; set; }
+    public decimal Quantidade { get; set; }
+    public int NumeroNF { get; set; }
+    public int SerieNF { get; set; }
+    public DateTime DataCompra { get; set; }
+    public string Usuario { get; set; }
+    public int IdEstoqueLocalDestino { get; set; }
+}
