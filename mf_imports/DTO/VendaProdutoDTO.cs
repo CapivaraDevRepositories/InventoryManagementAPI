8935284ccd81f2d85ad041c133af1d23045bb091
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-08-16 17:22:16 -0300

    criado DTO de venda e vendaproduto

---
+++ mf_imports/DTO/VendaProdutoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace mf_imports.DTO;
+
+public class VendaProdutoDTO
+{
+    public int ProdutoId { get; set; }
+    public int EstoqueLocalId { get; set; }
+    public decimal QuantidadeProduto { get; set; }
+}
