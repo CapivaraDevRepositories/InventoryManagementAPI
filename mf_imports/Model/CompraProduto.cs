84f0c737158efa92b08aec4d851e4e6a1d86d54b
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-08-16 12:30:58 -0300

    Adicionado novo model

---
+++ mf_imports/Model/CompraProduto.cs
@@ -0,0 +1,13 @@
+﻿namespace mf_imports.Model;
+
+public class CompraProduto
+{
+    public int Id { get; set; }
+    public int IdProduto { get; set; }
+    public Produto Produto { get; set; }
+    public decimal Quantidade { get; set; }
+    public int NumeroNF { get; set; }
+    public int SerieNF { get; set; }
+    public DateTime DataCompra { get; set; }
+    public string Usuario { get; set; }
+}
