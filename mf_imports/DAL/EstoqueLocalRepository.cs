858f2f068a772adb7af94703cefb4377ad1c1679
Marcio Rocha <dev.marcio.rocha@gmail.com>
2024-08-16 12:31:57 -0300

    adicionada pesquisa por nome para estoque local

---
+++ mf_imports/DAL/EstoqueLocalRepository.cs
@@ -0,0 +1,13 @@
+﻿using mf_imports.Model;
+
+namespace mf_imports.DAL;
+
+public class EstoqueLocalRepository : Repository<EstoqueLocal>
+{
+    public EstoqueLocalRepository(ConnectionContext context) : base(context) { }
+
+    public override IList<EstoqueLocal> GetByName(string name)
+    {
+        return _context.Set<EstoqueLocal>().Where(e => e.Nome.ToLower().Contains(name.ToLower())).ToList();
+    }
+}
